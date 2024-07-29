f419da51d56c454f84d77b7f10d679d22cdbd574
bertrammohr <bertram@mohr.nu>
2024-07-29 14:25:54 +0200

    cors + deleteFile + auth

--- index.js
+++ index.js
@@ -3,9 +3,20 @@ const multer = require('multer');
 const path = require('path');
 const fs = require('fs');
 const sharp = require('sharp');
+const dotenv = require('dotenv');
+const cors = require('cors');
+dotenv.config()
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+// CORS options
+const corsOptions = {
+    origin: process.env.CORS_ORIGIN
+};
+
+// Use the CORS middleware with the specified options
+app.use(cors(corsOptions));
 
 // Set up storage engine
 const storage = multer.memoryStorage(); // Store files in memory for processing
@@ -37,6 +48,18 @@ function checkFileType(file, cb) {
     }
 }
 
+const checkAuthorization = (req, res, next) => {
+    const authHeader = req.headers['authorization'];
+
+    if (authHeader && authHeader === process.env.API_KEY) {
+      next(); // Authorization header is correct, proceed to the next middleware or route handler
+    } else {
+      res.status(401).json({ error: 'Unauthorized' }); // Authorization header is incorrect, respond with 401 Unauthorized
+    }
+};
+
+const awaitedFileUploads = new Map();
+
 // Public folder
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
@@ -44,37 +67,117 @@ app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.get('/', (req, res) => res.send('Hello! Use /upload to upload an image.'));
 
 app.post('/upload', (req, res) => {
+    const imageName = req.query?.imageName;
+
+    // console.log("#1")
+
+    if (!imageName || !awaitedFileUploads.has(imageName)) {
+        // console.log("#2")
+        res.status(400).json({ error: 'No imageName or imageName is not allowed to post' });
+        return 
+    }
+
+    const uploadType = awaitedFileUploads.get(imageName);
+
+    // console.log("#3", uploadType, imageName);
+
     upload(req, res, async (err) => {
         if (err) {
+            // console.log("#4");
+            console.error(err);
             res.status(400).send(err);
         } else {
+            // console.log("#5")
             if (req.file == undefined) {
+                // console.log("#6")
                 res.status(400).send('Error: No File Selected!');
             } else {
+                // console.log("#7")
                 try {
+                    // console.log("#8")
                     const uploadDir = './uploads';
                     if (!fs.existsSync(uploadDir)) {
+                        // console.log("#9")
                         fs.mkdirSync(uploadDir);
                     }
+                    if (!fs.existsSync(path.join(uploadDir, uploadType))) {
+                        fs.mkdirSync(path.join(uploadDir, uploadType))
+                        // console.log("#9.5");
+                    }
 
-                    const filename = Date.now() + '.jpg';
-                    const filepath = path.join(uploadDir, filename);
+                    const filename = imageName + '.jpg';
+                    const filepath = path.join(uploadDir, uploadType, filename);
 
+                    // console.log("#10")
                     // Convert image to JPEG and save
+
+                    // console.log(req.file.buffer);
+
                     await sharp(req.file.buffer)
                         .jpeg()
                         .toFile(filepath);
-
+                    // console.log("#11")
                     res.json({
                         message: 'File uploaded and converted successfully!',
                         file: `uploads/${filename}`
                     });
                 } catch (error) {
+                    console.error(error);
+                    // console.log("#12");
                     res.status(500).send('Error processing image');
                 }
             }
         }
     });
 });
+
+app.get("/allowFileName", checkAuthorization, (req, res) => {
+    const imageName = req.query?.imageName;
+    const type = req.query?.type;
+
+    if (!imageName || !type) {
+        res.status(400).json({ error: 'Bad request' });
+        return 
+    }
+
+    // console.log(`Adding ${imageName} to the awaitedFileUploads`);
+
+    awaitedFileUploads.set(imageName, type);
+
+    setTimeout(() => {
+        awaitedFileUploads.delete(imageName);
+        // console.log(`Removed ${imageName} from the awaitedFileUploads`);
+    }, 60000)
+
+    res.sendStatus(200);
+})
+
+// TODO
+app.get("/deleteImage", checkAuthorization, (req, res) => {
+    const imageName = req.query?.imageName;
+    const type = req.query?.type;
+
+    // console.log("delete", imageName, type)
+
+    if (!imageName || !type) {
+        // console.log("400")
+        res.status(400).json({ error: 'Bad request' });
+        return
+    }
+
+    // Attempt to delete file
+
+    
+    try {
+        const imagePath = path.join("./uploads", type, imageName);
+        fs.unlinkSync(`${imagePath}.jpg`);
+        // console.log(`File ${imagePath} was deleted successfully`);
+        res.status(200);
+    } catch (err) {
+        console.log(`Error deleting file`);
+        console.error(err);
+        res.status(500);
+    }
+})
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
